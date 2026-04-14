6e83fda311f286024d1a15cd59da9f487d2e27ae
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-14 16:49:48 +0200

    simple shell 0.1

--- main.c
+++ main.c
@@ -11,12 +11,11 @@
  */
 int main(void)
 {
-	char *line = NULL;
+	char *line = NULL, *cmd;
 	size_t len = 0;
 	pid_t pid;
 	int status;
 	char *argv[2];
-	int i;
 
 	while (1)
 	{
@@ -33,27 +32,31 @@ int main(void)
 		/* remove newline */
 		line[strcspn(line, "\n")] = '\0';
 
+		/* skip leading spaces */
+		cmd = line;
+		while (*cmd == ' ' || *cmd == '\t')
+			cmd++;
+
 		/* remove trailing spaces */
-		i = strlen(line) - 1;
-		while (i >= 0 &&
-			(line[i] == ' ' || line[i] == '\t' || line[i] == '\n'))
+		int i = strlen(cmd) - 1;
+		while (i >= 0 && (cmd[i] == ' ' || cmd[i] == '\t'))
 		{
-			line[i] = '\0';
+			cmd[i] = '\0';
 			i--;
 		}
 
 		/* ignore empty line */
-		if (line[0] == '\0')
+		if (*cmd == '\0')
 			continue;
 
-		argv[0] = line;
+		argv[0] = cmd;
 		argv[1] = NULL;
 
 		pid = fork();
 
 		if (pid == 0)
 		{
-			if (execve(line, argv, NULL) == -1)
+			if (execve(cmd, argv, NULL) == -1)
 			{
 				perror("./hsh");
 				exit(1);
