3e0b47ac71c86c6c99932b37ba013fdecf0c1883
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-14 16:42:11 +0200

    simple shell 0.1

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * main - simple shell
@@ -11,47 +12,56 @@
  */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    pid_t pid;
-    int status;
-    char *argv[2];
+	char *line = NULL;
+	size_t len = 0;
+	pid_t pid;
+	int status;
+	char *argv[2];
+	int i;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf("($) ");
-        fflush(stdout);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("($) ");
 
-        if (getline(&line, &len, stdin) == -1)
-        {
-            if (isatty(STDIN_FILENO))
-                printf("\n");
-            break;
-        }
+		if (getline(&line, &len, stdin) == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
+		}
 
-        /* remove newline */
-        line[strcspn(line, "\n")] = '\0';
+		line[strcspn(line, "\n")] = '\0';
 
-        argv[0] = line;
-        argv[1] = NULL;
+		i = strlen(line) - 1;
+		while (i >= 0 && isspace(line[i]))
+		{
+			line[i] = '\0';
+			i--;
+		}
 
-        pid = fork();
+		if (line[0] == '\0')
+			continue;
 
-        if (pid == 0)
-        {
-            if (execve(line, argv, NULL) == -1)
-            {
-                perror("./hsh");
-                exit(1);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
+		argv[0] = line;
+		argv[1] = NULL;
 
-    free(line);
-    return (0);
+		pid = fork();
+
+		if (pid == 0)
+		{
+			if (execve(line, argv, NULL) == -1)
+			{
+				perror("./hsh");
+				exit(1);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+
+	free(line);
+	return (0);
 }
