db5b219826e99e12a650b4666045a53f118b2c4e
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-14 16:17:38 +0200

    Add main.c

--- main.c
+++ main.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 /**
- * main - point d'entrée du programme
+ * main - simple shell
  *
  * Return: 0
  */
 int main(void)
 {
-    printf("Hello, shell\n");
+    char *line = NULL;
+    size_t len = 0;
+    pid_t pid;
+    int status;
+    char *argv[2];
+
+    while (1)
+    {
+        printf("($) ");
+        fflush(stdout);
+
+        if (getline(&line, &len, stdin) == -1)
+        {
+            printf("\n");
+            break;
+        }
+
+        /* remove newline */
+        line[strcspn(line, "\n")] = '\0';
+
+        argv[0] = line;
+        argv[1] = NULL;
+
+        pid = fork();
+
+        if (pid == 0)
+        {
+            if (execve(line, argv, NULL) == -1)
+            {
+                perror("./hsh");
+                exit(1);
+            }
+        }
+        else
+        {
+            wait(&status);
+        }
+    }
+
+    free(line);
     return (0);
 }
