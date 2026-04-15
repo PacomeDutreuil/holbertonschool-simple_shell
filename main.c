c3c0d30cf461fdaad754b434cc7b731dbd0b40fe
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-15 16:40:47 +0200

    Add main.c

--- main.c
+++ main.c
@@ -1,120 +1,141 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
+#include "shell.h"
 
 /**
- * find_path - find command in PATH
+ * get_path - get PATH from environ
+ * @env: environment variables
+ * Return: PATH string
+ */
+char *get_path(char **env)
+{
+	int i = 0;
+
+	while (env[i])
+	{
+		if (strncmp(env[i], "PATH=", 5) == 0)
+			return (env[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * find_command - find executable in PATH
  * @cmd: command
- *
+ * @env: environment
  * Return: full path or NULL
  */
-char *find_path(char *cmd)
+char *find_command(char *cmd, char **env)
 {
-	char *path = getenv("PATH");
-	char *path_copy, *dir;
-	static char full_path[1024];
+	char *path, *path_copy, *token, *full_path;
+	struct stat st;
 
-	if (path == NULL)
+	if (cmd == NULL)
+		return (NULL);
+
+	/* if command already valid path */
+	if (stat(cmd, &st) == 0)
+		return (strdup(cmd));
+
+	path = get_path(env);
+	if (!path)
 		return (NULL);
 
 	path_copy = strdup(path);
-	if (path_copy == NULL)
+	if (!path_copy)
 		return (NULL);
 
-	dir = strtok(path_copy, ":");
-	while (dir)
+	token = strtok(path_copy, ":");
+	while (token)
 	{
-		sprintf(full_path, "%s/%s", dir, cmd);
-		if (access(full_path, X_OK) == 0)
+		full_path = malloc(strlen(token) + strlen(cmd) + 2);
+		if (!full_path)
+			return (free(path_copy), NULL);
+
+		sprintf(full_path, "%s/%s", token, cmd);
+
+		if (stat(full_path, &st) == 0)
 		{
 			free(path_copy);
 			return (full_path);
 		}
-		dir = strtok(NULL, ":");
+		free(full_path);
+		token = strtok(NULL, ":");
 	}
 
 	free(path_copy);
 	return (NULL);
 }
 
-/*
-main - simple shell with PATH 
-Return: 0
+/**
+ * main - simple shell
+ * @ac: argument count
+ * @av: argument vector
+ * @env: environment
+ * Return: 0
  */
-int main(void)
+int main(int ac, char **av, char **env)
 {
-	char *line = NULL, *cmd, *path_cmd;
+	char *line = NULL, *cmd_path = NULL;
 	size_t len = 0;
+	ssize_t read;
+	char *args[100];
+	int status, i;
 	pid_t pid;
-	int status;
-	char *argv[2];
-	int i;
+
+	(void)ac;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("($) ");
 
-		if (getline(&line, &len, stdin) == -1)
+		read = getline(&line, &len, stdin);
+		if (read == -1)
 		{
-			if (isatty(STDIN_FILENO))
-				printf("\n");
-			break;
+			free(line);
+			exit(0);
 		}
 
-		line[strcspn(line, "\n")] = '\0';
+		/* remove newline */
+		line[strcspn(line, "\n")] = 0;
 
-		cmd = line;
-		while (*cmd == ' ' || *cmd == '\t')
-			cmd++;
-
-		i = strlen(cmd) - 1;
-		while (i >= 0 && (cmd[i] == ' ' || cmd[i] == '\t'))
-		{
-			cmd[i] = '\0';
-			i--;
-		}
-
-		if (*cmd == '\0')
+		if (line[0] == '\0')
 			continue;
 
-		argv[0] = cmd;
-		argv[1] = NULL;
+		/* parse */
+		i = 0;
+		args[i] = strtok(line, " ");
+		while (args[i] && i < 99)
+			args[++i] = strtok(NULL, " ");
+		args[i] = NULL;
 
-		/* si chemin direct */
-		if (strchr(cmd, '/'))
+		if (strcmp(args[0], "exit") == 0)
 		{
-			if (access(cmd, X_OK) != 0)
-			{
-				perror("./hsh");
-				continue;
-			}
-			path_cmd = cmd;
+			free(line);
+			exit(0);
 		}
-		else
+
+		cmd_path = find_command(args[0], env);
+
+		if (!cmd_path)
 		{
-			path_cmd = find_path(cmd);
-			if (path_cmd == NULL)
-			{
-				perror("./hsh");
-				continue;
-			}
+			fprintf(stderr, "%s: 1: %s: not found\n", av[0], args[0]);
+			continue;
 		}
 
 		pid = fork();
-
 		if (pid == 0)
 		{
-			execve(path_cmd, argv, NULL);
-			perror("./hsh");
-			exit(1);
+			if (execve(cmd_path, args, env) == -1)
+			{
+				perror(av[0]);
+				exit(1);
+			}
 		}
 		else
-		{
 			wait(&status);
-		}
+
+		free(cmd_path);
 	}
 
 	free(line);
