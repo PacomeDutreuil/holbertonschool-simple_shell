f7d924113658bbfbdbd4333dae92d325708d997b
PacomeDutreuil <13007@holbertonstudents.com>
2026-04-15 17:00:05 +0200

    Add shell.h

--- shell.h
+++ shell.h
@@ -8,4 +8,17 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+/* PATH */
+char *get_path(char **env);
+char *find_command(char *cmd, char **env);
+
+/* EXEC */
+int execute_command(char *cmd_path, char **args, char **env, char *prog);
+
+/* PARSE */
+char **parse_line(char *line);
+
+/* UTILS */
+void free_args(char **args);
+
 #endif
